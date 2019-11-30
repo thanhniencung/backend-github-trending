1dd8fd6a24fb04a866f20e69473e01c1b7d49ee2
Kien Nguyen <ryan@exgo.vn>
2019-11-30 18:24:06 +0700

    connect to database

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module backend-github-trending
 go 1.13
 
 require (
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.2.0
 	golang.org/x/crypto v0.0.0-20191128160524-b544559bb6d1 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"backend-github-trending/db"
 	"backend-github-trending/handler"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+
+	sql := &db.Sql{
+		Host: "localhost",
+		Port: 5432,
+		UserName: "ryan",
+		Password: "postgres",
+		DbName: "golang",
+	}
+	sql.Connect()
+	defer sql.Close()
+
 	e := echo.New()
 	e.GET("/", handler.Welcome)
 
