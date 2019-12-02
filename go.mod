5aa760fc2ab33ec055fdba344d7effcf0d432670
Kien Nguyen <ryan@exgo.vn>
2019-12-02 17:55:08 +0700

    log file

--- go.mod
+++ go.mod
@@ -6,7 +6,12 @@ require (
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
+	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
+	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
 	github.com/lib/pq v1.2.0
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/rubenv/sql-migrate v0.0.0-20191121092708-da1cb182f00e // indirect
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/crypto v0.0.0-20191128160524-b544559bb6d1 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"backend-github-trending/db"
 	"backend-github-trending/handler"
-
+	log "backend-github-trending/log"
+	"context"
+	"fmt"
 	"github.com/labstack/echo"
+	"os"
 )
 
-func main() {
+func init() {
+	fmt.Println("init package main")
+	os.Setenv("APP_NAME", "github")
+	log.InitLogger(false)
+}
 
+func main() {
+	fmt.Println("main function")
 	sql := &db.Sql{
 		Host: "localhost",
 		Port: 5432,
@@ -19,6 +28,12 @@ func main() {
 	sql.Connect()
 	defer sql.Close()
 
+	var email string
+	err := sql.Db.GetContext(context.Background(), &email, "SELECT email FROM users WHERE email=$1", "abc@gmail.com")
+	if err != nil {
+		log.Error(err.Error())
+	}
+
 	e := echo.New()
 	e.GET("/", handler.Welcome)
 
@@ -27,3 +42,4 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
+
